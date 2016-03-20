cc05f4adbf57f061fdf30105821fee7f2af40609
Vladimir Gusev <vpacifist@yandex.ru>
2016-03-20 22:26:48 +0300

    Сделал заглушку на сайт

---
+++ www/comingsoon.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Coming Soon - Karabasville</title>
+  <link href='https://fonts.googleapis.com/css?family=Philosopher&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+  <style type="text/css">
+    html {
+      width: 100%;
+      height: 100%;
+    }
+    body {
+      width: 100%;
+      height: 100%;
+      background: url(img/bg_main.jpg) 0 0 no-repeat;
+      background-size: cover;
+      font-family: 'Philosopher', sans-serif;
+      font-size: 30px;
+      color: #444;
+      font-weight: bold;
+      margin: 0;
+      padding: 0;
+    }
+    .container {
+      width: 300px;
+      height: 256px;
+      margin: 0 auto;
+      padding: 90px 0;
+      text-align: center;
+      background: url(img/comingsoon-desk.png) 0 -70px no-repeat;
+      background-size: 100%;
+    }
+    img {
+        max-width: 80%;
+    }
+    p {
+      margin: 0;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <img src="img/logo2.png" alt="Logo">
+    <p>Coming Soon</p>
+  </div>
+</body>
+</html>
