f11ceff3db17c2800aba250e27a880f67cb50a84
Vladimir Gusev <vpacifist@yandex.ru>
2016-04-13 15:09:39 +0300

    Сделал целиком страницу Африки

---
+++ www/africa.php
@@ -0,0 +1,187 @@
+<?php include "header.php" ?>
+<link  href="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.css" rel="stylesheet"> <!-- 3 KB -->
+
+    <main>
+      <div class="container cat-demo">
+      <h1>HoneyDevil AFRICA</h1>
+      <a href="#cat-pedigree" class="anchor-link">Родословная</a>
+      <a href="#cat-show-res" class="anchor-link">Шоу результаты</a>
+        <div class="row">
+          <div class="col-lg-7">
+            <p>Окрас:  чёрная черепаха<br/>MCO f<br/>Дата рождения: 30.11.2015<br/>Заводчик: Елисеева Юлия<br/>Владелец: Журавлева Ольга</p>
+<!--             <p>Из описания экспертов:</p>
+            <p><b>Тело</b>: ***<br/><b>Глаза</b>: ***<br/><b>Уши</b>: ***<br/><b>Шерсть</b>: ***<br/><b>Хвост</b>: ***<br/>*** общая информация ***</p>
+ -->            <p>Генетический тест на гипертрофическую кардиомиопатию мейн-кунов ( HCM ) - N/N<br/>Генетический тест на аутосомно-доминантный поликистоз почек  ( ADPKD ) - N/N <br/>Генетический тест на дефицит пируваткиназы ( PKdef ) - N/N <br/>Генетический тест на аутосомно-рецессивную спинально-мышечную атрофию ( SMA ) - N/N<br/>Коронавирус (Canine/Feline Coronavirus/FIPV) – не обнаружено<br/>Панлейкопения (Virus panleukopenia Feline/ FPV) – не обнаружено<br/>Хламидиоз (Chlamydophila spp) – не обнаружено<br/>Токсоплазмоз (Toxoplasma gondii) – не обнаружено<br/>Патогенные микоплазмы кошек (M. felis, M. gateae) – не обнаружено<br>Грибы рода Trichophyton – не обнаружено<br>Грибы рода Microsporum – не обнаружено</p>
+          </div>
+          <!-- Personal Photos -->
+          <div class="col-lg-5 cat-demo-images">
+            <div class="fotorama"  data-nav="thumbs"  data-allowfullscreen="true">
+              <img src="img/africa/00-0075.jpg" data-caption="3 недели">
+              <img src="img/africa/00-0075-2.jpg" data-caption="3 недели">
+              <img src="img/africa/00-015.jpg" data-caption="1,5 месяца">
+              <img src="img/africa/00-025.jpg" data-caption="2,5 месяца">
+              <img src="img/africa/00-03.jpg" data-caption="3 месяца">
+              <img src="img/africa/00-035.jpg" data-caption="3,5 месяца">
+              <img src="img/africa/00-035-2.jpg" data-caption="3,5 месяца">
+              <img src="img/africa/00-04.jpg" data-caption="4 месяца">
+              <img src="img/africa/00-04-2.jpg" data-caption="4 месяца">
+            </div>
+          </div>
+          <!-- Pedigree -->
+          <div class="col-lg-12">
+            <h2 id="cat-pedigree">Родословная</h2>
+            <table class="cat-pedigree">
+              <tr>
+                <td rowspan="8"><p>DGCH (TICA) PL*Aldaker TAIPAN</p><p class="cat-pedigree-color">MCO d 23 03</p><a href="img/pedigree/africa-001.jpg" class="colorbox  pedigree-img  pedigree-img-1" title="КотОбормот"><img src="img/pedigree/africa-001.jpg"></a></td>
+                <td rowspan="4"><p>Supernova HITMAN</p><p class="cat-pedigree-color">MCO d 23 09</p><a href="img/pedigree/africa-011.jpg" class="colorbox  pedigree-img  pedigree-img-2" title="КотОбормот"><img src="img/pedigree/africa-011.jpg"></a></td>
+                <td rowspan="2"><p>Justcoons Cross Fire</p><p class="cat-pedigree-color">MCO d 09 23</p><a href="img/pedigree/africa-111.jpg" class="colorbox  pedigree-img  pedigree-img-3" title="КотОбормот"><img src="img/pedigree/africa-111.jpg"></a></td>
+                <td><p>Langstteich C.C. Juma</p><p class="cat-pedigree-color">MCO a 09 23</p></td>
+              </tr>
+              <tr>
+                <td><p>Langstteich DD'Elaine of Justcoons</p><p class="cat-pedigree-color">MCO f 22</p></td>
+              </tr>
+              <tr>
+                <td rowspan="2"><p>IT* Supernova Miss Costa Rica</p><p class="cat-pedigree-color">MCO n 09 23</p><a href="img/pedigree/africa-112.jpg" class="colorbox  pedigree-img  pedigree-img-3" title="КотОбормот"><img src="img/pedigree/africa-112.jpg"></a></td>
+                <td><p>DK*Olmocabe Selius</p><p class="cat-pedigree-color">MCO n 09 22</p></td>
+              </tr>
+              <tr>
+                <td><p>DK*Sebasco's Nova Scotia</p><p class="cat-pedigree-color">MCO f 23</p></td>
+              </tr>
+              <tr>
+                <td rowspan="4"><p>PL*Aldaker Ibiza</p><p class="cat-pedigree-color">MCO f 09</p><a href="img/pedigree/africa-012.jpg" class="colorbox  pedigree-img  pedigree-img-2" title="КотОбормот"><img src="img/pedigree/africa-012.jpg"></a></td>
+                <td rowspan="2"><p>Csák's Coon E Goran</p><p class="cat-pedigree-color">MCO d 09 22</p><a href="img/pedigree/africa-121.jpg" class="colorbox  pedigree-img  pedigree-img-3" title="КотОбормот"><img src="img/pedigree/africa-121.jpg"></a></td>
+                <td><p>GIC McKittycreek Reddy of Csák's Coon</p><p class="cat-pedigree-color">MCO d 09 22</p></td>
+              </tr>
+              <tr>
+                <td><p>Willowplace Merry of Csák's Coon</p><p class="cat-pedigree-color">MCO f 22</p></td>
+              </tr>
+              <tr>
+                <td rowspan="2"><p>FIN*Escape Flogging Molly</p><p class="cat-pedigree-color">MCO f 09 22</p><a href="img/pedigree/africa-122.jpg" class="colorbox  pedigree-img  pedigree-img-3" title="КотОбормот"><img src="img/pedigree/africa-122.jpg"></a></td>
+                <td><p>IC FIN*Escape's Urban Cowboy</p><p class="cat-pedigree-color">MCO n 09 22</p></td>
+              </tr>
+              <tr>
+                <td><p>DK*Wytopitlock Leria Kersil</p><p class="cat-pedigree-color">MCO g 22</p></td>
+              </tr>
+              <tr>
+                <td rowspan="8"><p>HoneyDevil Olive</p><p class="cat-pedigree-color">MCO n</p><a href="img/pedigree/africa-002.jpg" class="colorbox  pedigree-img  pedigree-img-1" title="КотОбормот"><img src="img/pedigree/africa-002.jpg"></a></td>
+                <td rowspan="4"><p>Int.Ch. DK Aloa's Esteban</p><p class="cat-pedigree-color">MCO a 03</p><a href="img/pedigree/africa-021.jpg" class="colorbox  pedigree-img  pedigree-img-2" title="КотОбормот"><img src="img/pedigree/africa-021.jpg"></a></td>
+                <td rowspan="2"><p>(N)Tojo Polaris Jim Morrison</p><p class="cat-pedigree-color">MCO d 09</p><a href="img/pedigree/africa-211.jpg" class="colorbox  pedigree-img  pedigree-img-3" title="КотОбормот"><img src="img/pedigree/africa-211.jpg"></a></td>
+                <td><p>Dotcom Ivor Novello</p><p class="cat-pedigree-color">MCO n 09</p></td>
+              </tr>
+              <tr>
+                <td><p>CH Tojo Polaris Victoria Beckham</p><p class="cat-pedigree-color">MCO f</p></td>
+              </tr>
+              <tr>
+                <td rowspan="2"><p>DK Aloa's Vindicator</p><p class="cat-pedigree-color">MCO f 09</p><a href="img/pedigree/africa-212.jpg" class="colorbox  pedigree-img  pedigree-img-3" title="КотОбормот"><img src="img/pedigree/africa-212.jpg"></a></td>
+                <td><p>DK Mountaineer's Beach Boy</p><p class="cat-pedigree-color">MCO e 09 22</p></td>
+              </tr>
+              <tr>
+                <td><p>CH DK Remind's Taramoon Cheyenne</p><p class="cat-pedigree-color">MCO ns 22</p></td>
+              </tr>
+              <tr>
+                <td rowspan="4"><p>A Winerau's Dream</p><p class="cat-pedigree-color">MCO f</p><a href="img/pedigree/africa-022.jpg" class="colorbox  pedigree-img  pedigree-img-2" title="КотОбормот"><img src="img/pedigree/africa-022.jpg"></a></td>
+                <td rowspan="2"><p>A Winerau's Yankee</p><p class="cat-pedigree-color">MCO n 22</p><a href="img/pedigree/africa-221.jpg" class="colorbox  pedigree-img  pedigree-img-3" title="КотОбормот"><img src="img/pedigree/africa-221.jpg"></a></td>
+                <td><p>IC Quiggley of Magic Lake</p><p class="cat-pedigree-color">MCO d 22</p></td>
+              </tr>
+              <tr>
+                <td><p>CH(N) Mydele's Edda</p><p class="cat-pedigree-color">MCO n 22</p></td>
+              </tr>
+              <tr>
+                <td rowspan="2"><p>CH Justcoons Crazy Diamond (CFG)</p><p class="cat-pedigree-color">MCO f</p><a href="img/pedigree/africa-222.jpg" class="colorbox  pedigree-img  pedigree-img-3" title="КотОбормот"><img src="img/pedigree/africa-222.jpg"></a></td>
+                <td><p>DK Aloa's Nifty Wolf</p><p class="cat-pedigree-color">MCO as</p></td>
+              </tr>
+              <tr>
+                <td><p>Langstteich's No'Velle</p><p class="cat-pedigree-color">MCO f</p></td>
+              </tr>
+            </table>
+          </div>
+<!--           <div class="col-lg-12">
+            <h2 id="cat-show-res">Шоу результаты</h2>
+            <table class="show-results">
+              <tr>
+                <th>Date / Дата</th> <th>Show / Выставка</th> <th>Judge / Эксперты</th> <th>Results / Результаты</th>
+              </tr>
+              <tr>
+                <td>11.07.2015</td> <td>Санкт-Петербург, Всеволожск, «Котофей на даче»</td> <td>Olga Mironova</td> <td>EX1, nom BIS</td>
+              </tr>
+              <tr>
+                <td>12.07.2015</td> <td>Санкт-Петербург, Всеволожск, «Котофей на даче»</td> <td>Tamara Sapozhnikova</td> <td>EX1, nom BIS, <span class="color-red">WCF Ring Place 10</span></td>
+              </tr>
+              <tr>
+                <td>18.07.2015</td> <td>Санкт-Петербург, международная выставка кошек</td> <td>Elena Butomova</td> <td>EX1, nom BIS, <span class="color-red">Best Kitten breed show</span></td>
+              </tr>
+              <tr>
+                <td>19.07.2015</td> <td>Санкт-Петербург, международная выставка кошек</td> <td><span class="color-red">Wojciech-Albert Kurkowski, Poland</span></td> <td>EX1, nom BIS, <span class="color-red">WCF Ring Place 10</span></td>
+              </tr>
+              <tr><td colspan="4" class="text-center"><span class="color-red">Закрыт титул Petit Champion – PCh WCF / Котенок Чемпион</span></td></tr>
+              <tr>
+                <td>21.11.2015</td> <td>Санкт-Петербург, «ЗооШоу 2015»</td> <td>Екатерина Дудина</td> <td>EX1, nom BIS</td>
+              </tr>
+              <tr>
+                <td>22.11.2015</td> <td>Санкт-Петербург, «ЗооШоу 2015»</td> <td>Elena Ignatova</td> <td>EX1, nom BIS</td>
+              </tr>
+              <tr>
+                <td>12.12.2015</td> <td>Санкт-Петербург, «Рождественские чудеса»</td> <td>Tatiana Deulenko</td> <td>EX1, nom BIS</td>
+              </tr>
+              <tr>
+                <td>12.12.2015</td> <td>Санкт-Петербург, «Рождественские чудеса»</td> <td>Olga Mironova</td>  <td>EX1</td>
+              </tr>
+              <tr><td colspan="4" class="text-center"><span class="color-red">Закрыт титул Jnior Champion – JCh WCF / Юниор Чемпион</span></td></tr>
+              <tr>
+                <td>19.12.2015</td> <td>Санкт-Петербург, «В гостях у Саффи и Фелисити»</td> <td class="color-red">van der Wijk Klaas, Netherlands</td> <td>EX1, nom BIS, <span class="color-red">Best opposite Sex Adult breed show</span></td>
+              </tr>
+              <tr>
+                <td>20.12.2015</td> <td>Санкт-Петербург, «В гостях у Саффи и Фелисити»</td> <td class="color-red">Natalia Byelova, Ukraine</td> <td>EX1, nom BIS, <span class="color-red">Best opposite Sex</span></td>
+              </tr>
+              <tr>
+                <td>16.01.2016</td> <td>Санкт-Петербург, «Зимняя сказка»</td> <td class="color-red">Andreas Mobius, Germany</td> <td>EX1, nom BIS, <span class="color-red">Best opposite Sex Adult breed show</span></td>
+              </tr>
+              <tr>
+                <td>17.01.2016</td> <td>Санкт-Петербург, «Зимняя сказка»</td> <td>Elena Butomova</td>  <td>EX1</td>
+              </tr>
+              <tr><td colspan="4" class="text-center"><span class="color-red">Закрыт титул Champion – Ch WCF / Чемпион WCF</span></td></tr>
+              <tr>
+                <td>06.02.2016</td> <td>Великий Новгород, «Кубок Великого Новгорода»</td> <td>Тамара Емельянова</td> <td>EX1</span></td>
+              </tr>
+              <tr>
+                <td>07.02.2016</td> <td>Великий Новгород, «Кубок Великого Новгорода»</td> <td>Татьяна Есина</td>  <td>EX1</td>
+              </tr>
+              <tr><td colspan="4" class="text-center"><span class="color-red">Закрыт титул International Champion – ICh WCF / Международный Чемпион WCF</span></td></tr>
+              <tr>
+                <td>05.03.2016</td> <td>Санкт-Петербург, "Весенний марафон"</td>  <td>Елена Федоренко</td>  <td>EX1, nom BIS</td>
+              </tr>
+              <tr>
+                <td>06.03.2016</td> <td>Санкт-Петербург, "Весенний марафон"</td>  <td>Тамара Гулянская</td>  <td>EX1</td>
+              </tr>
+              <tr>
+                <td>07.03.2016</td> <td>Санкт-Петербург, "Весенний марафон"</td>  <td>Elena Ignatova</td>  <td>EX1</td>
+              </tr>
+              <tr>
+                <td>07.03.2016</td> <td>Санкт-Петербург, "Весенний марафон"</td>  <td>Anna Nazarova</td>  <td>EX1</td>
+              </tr>
+              <tr><td colspan="4" class="text-center"><span class="color-red">Закрыт титул Grand International Champion – GICh WCF / Большой Международный Чемпион WCF и открыл титул European Champion /Европейский ЧемпионWCF</span></td></tr>
+            </table>
+          </div>
+ -->        </div>
+      </div>
+    </main>
+
+    <?php include "footer.php" ?>
+
+    <script src="js/jquery-1.12.0.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.js"></script> <!-- 16 KB -->
+    <script src="js/jquery.colorbox-min.js"></script>
+    <script>
+      $( document ).ready(function() {
+        $('a.colorbox').colorbox({
+          // rel:$(this).attr('src'),
+          rel:"colorbox",
+          innerWidth:"90%",
+          innerHeight:"90%"
+        });
+      });
+    </script>
+
+
+  </body>
+</html>
