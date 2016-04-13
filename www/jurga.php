fe9caa55e236dacec07a5781f04d8ea828e80f42
Vladimir Gusev <vpacifist@yandex.ru>
2016-04-13 15:09:53 +0300

    Сделал целиком страницу Юрги

---
+++ www/jurga.php
@@ -0,0 +1,187 @@
+<?php include "header.php" ?>
+<link  href="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.css" rel="stylesheet"> <!-- 3 KB -->
+
+    <main>
+      <div class="container cat-demo">
+      <h1>RASCOON JURGA</h1>
+      <a href="#cat-pedigree" class="anchor-link">Родословная</a>
+      <a href="#cat-show-res" class="anchor-link">Шоу результаты</a>
+        <div class="row">
+          <div class="col-lg-7">
+            <p>Окрас:  чёрная мраморная черепаха<br/>MCO f 22<br/>Дата рождения: 9.12.2015<br/>Заводчик: Рассыпина Светлана <br/>Владелец: Журавлева Ольга</p>
+<!--             <p>Из описания экспертов:</p>
+            <p><b>Тело</b>: ***<br/><b>Глаза</b>: ***<br/><b>Уши</b>: ***<br/><b>Шерсть</b>: ***<br/><b>Хвост</b>: ***<br/>*** общая информация ***</p>
+ -->            <p>Генетический тест на гипертрофическую кардиомиопатию мейн-кунов ( HCM ) - N/N<br/>Генетический тест на аутосомно-доминантный поликистоз почек  ( ADPKD ) - N/N <br/>Генетический тест на дефицит пируваткиназы ( PKdef ) - N/N <br/>Генетический тест на аутосомно-рецессивную спинально-мышечную атрофию ( SMA ) - N/N<br/>Коронавирус (Canine/Feline Coronavirus/FIPV) – не обнаружено<br/>Панлейкопения (Virus panleukopenia Feline/ FPV) – не обнаружено<br/>Хламидиоз (Chlamydophila spp) – не обнаружено<br/>Токсоплазмоз (Toxoplasma gondii) – не обнаружено<br/>Патогенные микоплазмы кошек (M. felis, M. gateae) – не обнаружено<br>Грибы рода Trichophyton – не обнаружено<br>Грибы рода Microsporum – не обнаружено</p>
+          </div>
+          <!-- Personal Photos -->
+          <div class="col-lg-5 cat-demo-images">
+            <div class="fotorama"  data-nav="thumbs"  data-allowfullscreen="true">
+              <img src="img/jurga/00-0009.jpg" data-caption="9 дней">
+              <img src="img/jurga/00-01.jpg" data-caption="1 месяц">
+              <img src="img/jurga/00-021.jpg" data-caption="2 месяца">
+              <img src="img/jurga/00-03.jpg" data-caption="3 месяца">
+              <img src="img/jurga/00-03-2.jpg" data-caption="3 месяца">
+              <img src="img/jurga/00-03-3.jpg" data-caption="3 месяца">
+              <img src="img/jurga/00-035.jpg" data-caption="3,5 месяца">
+              <img src="img/jurga/00-035-2.jpg" data-caption="3,5 месяца">
+              <img src="img/jurga/00-035-3.jpg" data-caption="3,5 месяца">
+            </div>
+          </div>
+          <!-- Pedigree -->
+          <div class="col-lg-12">
+            <h2 id="cat-pedigree">Родословная</h2>
+            <table class="cat-pedigree">
+              <tr>
+                <td rowspan="8"><p>Pillowtalk`s Commander</p><p class="cat-pedigree-color">MCO black white</p><a href="img/pedigree/jurga-001.jpg" class="colorbox  pedigree-img  pedigree-img-1" title="КотОбормот"><img src="img/pedigree/jurga-001.jpg"></a></td>
+                <td rowspan="4"><p>Elphinstone Pushkin</p><p class="cat-pedigree-color">MCO black white</p><a href="img/pedigree/jurga-011.jpg" class="colorbox  pedigree-img  pedigree-img-2" title="КотОбормот"><img src="img/pedigree/jurga-011.jpg"></a></td>
+                <td rowspan="2"><p>Ch. Justcoons Peak Asso</p><p class="cat-pedigree-color">brown classic tabby</p><a href="img/pedigree/jurga-111.jpg" class="colorbox  pedigree-img  pedigree-img-3" title="КотОбормот"><img src="img/pedigree/jurga-111.jpg"></a></td>
+                <td><p>Vens Passion Kaiser Barbarosa</p><p class="cat-pedigree-color">red silver classic tabby</p></td>
+              </tr>
+              <tr>
+                <td><p>Justcoons Ode to Elize</p><p class="cat-pedigree-color">black white</p></td>
+              </tr>
+              <tr>
+                <td rowspan="2"><p>Elphinstone Heaven</p><p class="cat-pedigree-color">brown classic tabby white</p><a href="img/pedigree/jurga-112.jpg" class="colorbox  pedigree-img  pedigree-img-3" title="КотОбормот"><img src="img/pedigree/jurga-112.jpg"></a></td>
+                <td><p>Pirat Black od Vajgaru*CZ</p><p class="cat-pedigree-color">black silver classic tabby</p></td>
+              </tr>
+              <tr>
+                <td><p>Pillowtalk's Jambalaya</p><p class="cat-pedigree-color">blue classic tabby white</p></td>
+              </tr>
+              <tr>
+                <td rowspan="4"><p>Pillowtalk´s Tara</p><p class="cat-pedigree-color">MCO brown torbie</p><a href="img/pedigree/jurga-012.jpg" class="colorbox  pedigree-img  pedigree-img-2" title="КотОбормот"><img src="img/pedigree/jurga-012.jpg"></a></td>
+                <td rowspan="2"><p>Cool Mation´s Angel Sky</p><p class="cat-pedigree-color">black</p><a href="img/pedigree/jurga-121.jpg" class="colorbox  pedigree-img  pedigree-img-3" title="КотОбормот"><img src="img/pedigree/jurga-121.jpg"></a></td>
+                <td><p>Pillowtalk´s Xtro Energie</p><p class="cat-pedigree-color">brown classic tabby</p></td>
+              </tr>
+              <tr>
+                <td><p>Timberhills Chelsea</p><p class="cat-pedigree-color">black  tortie</p></td>
+              </tr>
+              <tr>
+                <td rowspan="2"><p>Pillowtalk´s United Colour</p><p class="cat-pedigree-color">brown classic torbie</p><a href="img/pedigree/jurga-122.jpg" class="colorbox  pedigree-img  pedigree-img-3" title="КотОбормот"><img src="img/pedigree/jurga-122.jpg"></a></td>
+                <td><p>Cool Motion´s Chipsy</p><p class="cat-pedigree-color">brown classic tabby white</p></td>
+              </tr>
+              <tr>
+                <td><p>Pillowtalk's Velvet</p><p class="cat-pedigree-color">blue classic torbie</p></td>
+              </tr>
+              <tr>
+                <td rowspan="8"><p>CH. LongTips Altziona</p><p class="cat-pedigree-color">MCO f 22</p><a href="img/pedigree/jurga-002.jpg" class="colorbox  pedigree-img  pedigree-img-1" title="КотОбормот"><img src="img/pedigree/jurga-002.jpg"></a></td>
+                <td rowspan="4"><p>Favorite Angel Oxford</p><p class="cat-pedigree-color">MCO d 22 09</p><a href="img/pedigree/jurga-021.jpg" class="colorbox  pedigree-img  pedigree-img-2" title="КотОбормот"><img src="img/pedigree/jurga-021.jpg"></a></td>
+                <td rowspan="2"><p>Golden Soul Sherchan</p><p class="cat-pedigree-color">MCO d 22</p><a href="img/pedigree/jurga-211.jpg" class="colorbox  pedigree-img  pedigree-img-3" title="КотОбормот"><img src="img/pedigree/jurga-211.jpg"></a></td>
+                <td><p>WCh Dynamitcats Snow Survivor</p><p class="cat-pedigree-color">MCO w 62</p></td>
+              </tr>
+              <tr>
+                <td><p>Lights of Moscow Vivien Lee</p><p class="cat-pedigree-color">MCO d 22 03</p></td>
+              </tr>
+              <tr>
+                <td rowspan="2"><p>Moscow Star`s Lady Winter</p><p class="cat-pedigree-color">MCO f 22 03</p><a href="img/pedigree/jurga-212.jpg" class="colorbox  pedigree-img  pedigree-img-3" title="КотОбормот"><img src="img/pedigree/jurga-212.jpg"></a></td>
+                <td><p>ECh Gentle Giant`s Dizzi Rapture</p><p class="cat-pedigree-color">MCO n 23</p></td>
+              </tr>
+              <tr>
+                <td><p>GICh Golden Soul Camomile</p><p class="cat-pedigree-color">MCO d 23 03</p></td>
+              </tr>
+              <tr>
+                <td rowspan="4"><p>Gamma Long Meadow</p><p class="cat-pedigree-color">MCO f 24 09/p><a href="img/pedigree/jurga-022.jpg" class="colorbox  pedigree-img  pedigree-img-2" title="КотОбормот"><img src="img/pedigree/jurga-022.jpg"></a></td>
+                <td rowspan="2"><p>Ch Coolmotion`s Amarango</p><p class="cat-pedigree-color">MCO n</p><a href="img/pedigree/jurga-221.jpg" class="colorbox  pedigree-img  pedigree-img-3" title="КотОбормот"><img src="img/pedigree/jurga-221.jpg"></a></td>
+                <td><p>Pillowtalk`s Xtro Energie</p><p class="cat-pedigree-color">MCO n 22</p></td>
+              </tr>
+              <tr>
+                <td><p>Timberhills Chelsea</p><p class="cat-pedigree-color">MCO f</p></td>
+              </tr>
+              <tr>
+                <td rowspan="2"><p>Baila Long Meadow</p><p class="cat-pedigree-color">MCO f 24 09</p><a href="img/pedigree/jurga-222.jpg" class="colorbox  pedigree-img  pedigree-img-3" title="КотОбормот"><img src="img/pedigree/jurga-222.jpg"></a></td>
+                <td><p>ICh La-Mansh Krasnyj Dar</p><p class="cat-pedigree-color">MCO d 23 09</p></td>
+              </tr>
+              <tr>
+                <td><p>Ch Coolmotion`s Naomi</p><p class="cat-pedigree-color">MCO n 22</p></td>
+              </tr>
+            </table>
+          </div>
+<!--           <div class="col-lg-12">
+            <h2 id="cat-show-res">Шоу результаты</h2>
+            <table class="show-results">
+              <tr>
+                <th>Date / Дата</th> <th>Show / Выставка</th> <th>Judge / Эксперты</th> <th>Results / Результаты</th>
+              </tr>
+              <tr>
+                <td>11.07.2015</td> <td>Санкт-Петербург, Всеволожск, «Котофей на даче»</td> <td>Olga Mironova</td> <td>EX1, nom BIS</td>
+              </tr>
+              <tr>
+                <td>12.07.2015</td> <td>Санкт-Петербург, Всеволожск, «Котофей на даче»</td> <td>Tamara Sapozhnikova</td> <td>EX1, nom BIS, <span class="color-red">WCF Ring Place 10</span></td>
+              </tr>
+              <tr>
+                <td>18.07.2015</td> <td>Санкт-Петербург, международная выставка кошек</td> <td>Elena Butomova</td> <td>EX1, nom BIS, <span class="color-red">Best Kitten breed show</span></td>
+              </tr>
+              <tr>
+                <td>19.07.2015</td> <td>Санкт-Петербург, международная выставка кошек</td> <td><span class="color-red">Wojciech-Albert Kurkowski, Poland</span></td> <td>EX1, nom BIS, <span class="color-red">WCF Ring Place 10</span></td>
+              </tr>
+              <tr><td colspan="4" class="text-center"><span class="color-red">Закрыт титул Petit Champion – PCh WCF / Котенок Чемпион</span></td></tr>
+              <tr>
+                <td>21.11.2015</td> <td>Санкт-Петербург, «ЗооШоу 2015»</td> <td>Екатерина Дудина</td> <td>EX1, nom BIS</td>
+              </tr>
+              <tr>
+                <td>22.11.2015</td> <td>Санкт-Петербург, «ЗооШоу 2015»</td> <td>Elena Ignatova</td> <td>EX1, nom BIS</td>
+              </tr>
+              <tr>
+                <td>12.12.2015</td> <td>Санкт-Петербург, «Рождественские чудеса»</td> <td>Tatiana Deulenko</td> <td>EX1, nom BIS</td>
+              </tr>
+              <tr>
+                <td>12.12.2015</td> <td>Санкт-Петербург, «Рождественские чудеса»</td> <td>Olga Mironova</td>  <td>EX1</td>
+              </tr>
+              <tr><td colspan="4" class="text-center"><span class="color-red">Закрыт титул Jnior Champion – JCh WCF / Юниор Чемпион</span></td></tr>
+              <tr>
+                <td>19.12.2015</td> <td>Санкт-Петербург, «В гостях у Саффи и Фелисити»</td> <td class="color-red">van der Wijk Klaas, Netherlands</td> <td>EX1, nom BIS, <span class="color-red">Best opposite Sex Adult breed show</span></td>
+              </tr>
+              <tr>
+                <td>20.12.2015</td> <td>Санкт-Петербург, «В гостях у Саффи и Фелисити»</td> <td class="color-red">Natalia Byelova, Ukraine</td> <td>EX1, nom BIS, <span class="color-red">Best opposite Sex</span></td>
+              </tr>
+              <tr>
+                <td>16.01.2016</td> <td>Санкт-Петербург, «Зимняя сказка»</td> <td class="color-red">Andreas Mobius, Germany</td> <td>EX1, nom BIS, <span class="color-red">Best opposite Sex Adult breed show</span></td>
+              </tr>
+              <tr>
+                <td>17.01.2016</td> <td>Санкт-Петербург, «Зимняя сказка»</td> <td>Elena Butomova</td>  <td>EX1</td>
+              </tr>
+              <tr><td colspan="4" class="text-center"><span class="color-red">Закрыт титул Champion – Ch WCF / Чемпион WCF</span></td></tr>
+              <tr>
+                <td>06.02.2016</td> <td>Великий Новгород, «Кубок Великого Новгорода»</td> <td>Тамара Емельянова</td> <td>EX1</span></td>
+              </tr>
+              <tr>
+                <td>07.02.2016</td> <td>Великий Новгород, «Кубок Великого Новгорода»</td> <td>Татьяна Есина</td>  <td>EX1</td>
+              </tr>
+              <tr><td colspan="4" class="text-center"><span class="color-red">Закрыт титул International Champion – ICh WCF / Международный Чемпион WCF</span></td></tr>
+              <tr>
+                <td>05.03.2016</td> <td>Санкт-Петербург, "Весенний марафон"</td>  <td>Елена Федоренко</td>  <td>EX1, nom BIS</td>
+              </tr>
+              <tr>
+                <td>06.03.2016</td> <td>Санкт-Петербург, "Весенний марафон"</td>  <td>Тамара Гулянская</td>  <td>EX1</td>
+              </tr>
+              <tr>
+                <td>07.03.2016</td> <td>Санкт-Петербург, "Весенний марафон"</td>  <td>Elena Ignatova</td>  <td>EX1</td>
+              </tr>
+              <tr>
+                <td>07.03.2016</td> <td>Санкт-Петербург, "Весенний марафон"</td>  <td>Anna Nazarova</td>  <td>EX1</td>
+              </tr>
+              <tr><td colspan="4" class="text-center"><span class="color-red">Закрыт титул Grand International Champion – GICh WCF / Большой Международный Чемпион WCF и открыл титул European Champion /Европейский ЧемпионWCF</span></td></tr>
+            </table>
+          </div>
+ -->        </div>
+      </div>
+    </main>
+
+    <?php include "footer.php" ?>
+
+    <script src="js/jquery-1.12.0.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.js"></script> <!-- 16 KB -->
+    <script src="js/jquery.colorbox-min.js"></script>
+    <script>
+      $( document ).ready(function() {
+        $('a.colorbox').colorbox({
+          // rel:$(this).attr('src'),
+          rel:"colorbox",
+          innerWidth:"90%",
+          innerHeight:"90%"
+        });
+      });
+    </script>
+
+
+  </body>
+</html>
