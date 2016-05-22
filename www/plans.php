74769862bd706dfaba69fd428e82b22dcb087c96
Vladimir Gusev <vpacifist@yandex.ru>
2016-05-22 09:33:56 +0300

    Добавил родословную помёта

--- www/plans.php
+++ www/plans.php
@@ -25,15 +25,69 @@
           <div style='display:none'>
             <div id="plans-a-brood-pedigree" class="brood-pedigree">
               <h2>Родословная</h2>
-              <table>
-                <tr>  <td rowspan="4">Имя кота</td>  <td rowspan="2">Имя кота</td>  <td>Имя кота</td>  </tr>
-                <tr>                                                                <td>Имя кота</td>  </tr>
-                <tr>                                 <td rowspan="2">Имя кота</td>  <td>Имя кота</td>  </tr>
-                <tr>                                                                <td>Имя кота</td>  </tr>
-                <tr>  <td rowspan="4">Имя кота</td>  <td rowspan="2">Имя кота</td>  <td>Имя кота</td>  </tr>
-                <tr>                                                                <td>Имя кота</td>  </tr>
-                <tr>                                 <td rowspan="2">Имя кота</td>  <td>Имя кота</td>  </tr>
-                <tr>                                                                <td>Имя кота</td>  </tr>
+              <table class="cat-pedigree">
+                <tr>
+                  <td rowspan="8"><p>GICh (WCF) Captain Coon’s Aristotel</p><p class="cat-pedigree-color">MCO d 03 22</p><a href="img/tosha.jpg" class="colorbox  pedigree-img  pedigree-img-1" title="GICh (WCF) Captain Coon’s Aristotel"><img src="img/tosha.jpg"></a></td>
+                  <td rowspan="4"><p>Ch. Pocoloco Villa Park*PL</p><p class="cat-pedigree-color">MCO n 09 22</p><a href="img/pedigree/tosha-001.jpg" class="colorbox  pedigree-img  pedigree-img-2" title="Ch. Pocoloco Villa Park*PL"><img src="img/pedigree/tosha-001.jpg"></a></td>
+                  <td rowspan="2"><p>IC Immanuele Villa Park*PL</p><p class="cat-pedigree-color">MCO ns 03</p><a href="img/pedigree/tosha-011.jpg" class="colorbox  pedigree-img  pedigree-img-3" title="IC Immanuele Villa Park*PL"><img src="img/pedigree/tosha-011.jpg"></a></td>
+                  <td><p>CH PL*Gatta Fumetto Argentino</p><p class="cat-pedigree-color">MCO ns 09</p></td>
+                </tr>
+                <tr>
+                  <td><p>Dreamy of Saxonyforest</p><p class="cat-pedigree-color">MCO n 09 23</p></td>
+                </tr>
+                <tr>
+                  <td rowspan="2"><p>Bella Villa Park*PL</p><p class="cat-pedigree-color">MCO n 22</p><a href="img/pedigree/tosha-012.jpg" class="colorbox  pedigree-img  pedigree-img-3" title="Bella Villa Park*PL"><img src="img/pedigree/tosha-012.jpg"></a></td>
+                  <td><p>IC Escudo Velvet Rose*PL, JW</p><p class="cat-pedigree-color">MCO n 22</p></td>
+                </tr>
+                <tr>
+                  <td><p>Ch. Goldie Rose Villa Park*PL</p><p class="cat-pedigree-color">MCO f 22</p></td>
+                </tr>
+                <tr>
+                  <td rowspan="4"><p>DK Aloa's La Luna</p><p class="cat-pedigree-color">MCO e 22</p><a href="img/pedigree/tosha-002.jpg" class="colorbox  pedigree-img  pedigree-img-2" title="DK Aloa's La Luna"><img src="img/pedigree/tosha-002.jpg"></a></td>
+                  <td rowspan="2"><p>(N)Tojo Polaris Jim Morisson</p><p class="cat-pedigree-color">MCO d 09</p><a href="img/pedigree/tosha-021.jpg" class="colorbox  pedigree-img  pedigree-img-3" title="(N)Tojo Polaris Jim Morisson"><img src="img/pedigree/tosha-021.jpg"></a></td>
+                  <td><p>DotCom Ivor Novello</p><p class="cat-pedigree-color">MCO n 09</p></td>
+                </tr>
+                <tr>
+                  <td><p>CH(N) Tojo Polaris Victoria Beckham</p><p class="cat-pedigree-color">MCO f</p></td>
+                </tr>
+                <tr>
+                  <td rowspan="2"><p>DK Aloa's Tutti Frutti</p><p class="cat-pedigree-color">MCO g 22</p><a href="img/pedigree/tosha-022.jpg" class="colorbox  pedigree-img  pedigree-img-3" title="DK Aloa's Tutti Frutti"><img src="img/pedigree/tosha-022.jpg"></a></td>
+                  <td><p>DK Mountaineer's Beach Boy</p><p class="cat-pedigree-color">MCO e 09 22</p></td>
+                </tr>
+                <tr>
+                  <td><p>DK Mountaineer's Wannaway Jezebel</p><p class="cat-pedigree-color">MCO g 22</p></td>
+                </tr>
+                <tr>
+                  <td rowspan="8"><p>GICh (WCF) Solar Wind Charming Lady</p><p class="cat-pedigree-color">MCO ns 22</p><a href="img/tigra.jpg" class="colorbox  pedigree-img  pedigree-img-1" title="GICh (WCF) Solar Wind Charming Lady"><img src="img/tigra.jpg"></a></td>
+                  <td rowspan="4"><p>Ch. Five Star's Alehandro</p><p class="cat-pedigree-color">MCO as 22</p><a href="img/pedigree/tigra-001.jpg" class="colorbox  pedigree-img  pedigree-img-2" title="Ch. Five Star's Alehandro"><img src="img/pedigree/tigra-001.jpg"></a></td>
+                  <td rowspan="2"><p>G.I.Ch. Sharm's World Tornado Blue</p><p class="cat-pedigree-color">MCO a</p><a href="img/pedigree/tigra-011.jpg" class="colorbox  pedigree-img  pedigree-img-3" title="G.I.Ch. Sharm's World Tornado Blue"><img src="img/pedigree/tigra-011.jpg"></a></td>
+                  <td><p>Sharm's World Madagascar</p><p class="cat-pedigree-color">MCO ds 03</p></td>
+                </tr>
+                <tr>
+                  <td><p>Sharm's World la Valetta</p><p class="cat-pedigree-color">MCO f</p></td>
+                </tr>
+                <tr>
+                  <td rowspan="2"><p>Well Being Demi Mur</p><p class="cat-pedigree-color">MCO as 22</p><a href="img/pedigree/tigra-012.jpg" class="colorbox  pedigree-img  pedigree-img-3" title="Well Being Demi Mur"><img src="img/pedigree/tigra-012.jpg"></a></td>
+                  <td><p>E.Ch.Buffalo Krasnyi Dar*RU</p><p class="cat-pedigree-color">MCO a 22</p></td>
+                </tr>
+                <tr>
+                  <td><p>G.I.Ch. UfaCoon's Naomi</p><p class="cat-pedigree-color">MCO as 22</p></td>
+                </tr>
+                <tr>
+                  <td rowspan="4"><p>Ch. Solar Wind Victory</p><p class="cat-pedigree-color">MCO n 22</p><a href="img/pedigree/tigra-002.jpg" class="colorbox  pedigree-img  pedigree-img-2" title="Ch. Solar Wind Victory"><img src="img/pedigree/tigra-002.jpg"></a></td>
+                  <td rowspan="2"><p>G.I.Ch. Ursus Black Goldjasmine Afonya Coon*RU</p><p class="cat-pedigree-color">MCO n</p><a href="img/pedigree/tigra-021.jpg" class="colorbox  pedigree-img  pedigree-img-3" title="G.I.Ch. Ursus Black Goldjasmine Afonya Coon*RU"><img src="img/pedigree/tigra-021.jpg"></a></td>
+                  <td><p>Goldjasmine Ferrari</p><p class="cat-pedigree-color">MCO d 22</p></td>
+                </tr>
+                <tr>
+                  <td><p>Ch.(FIFe) Infiniti Perfeet Cat*UA</p><p class="cat-pedigree-color">MCO n</p></td>
+                </tr>
+                <tr>
+                  <td rowspan="2"><p>Ch. Riddlecoon G Starquality Miata</p><p class="cat-pedigree-color">MCO fs 22</p><a href="img/pedigree/tigra-022.jpg" class="colorbox  pedigree-img  pedigree-img-3" title="Ch. Riddlecoon G Starquality Miata"><img src="img/pedigree/tigra-022.jpg"></a></td>
+                  <td><p>Toreador Vom Pommerland Delicat</p><p class="cat-pedigree-color">MCO es</p></td>
+                </tr>
+                <tr>
+                  <td><p>QGC (TICA), Ch. Riddlecoon's L.E.Esti</p><p class="cat-pedigree-color">MCO n 22 03</p></td>
+                </tr>
               </table>
             </div>
           </div>
@@ -50,7 +104,20 @@
     <script src="js/jquery.colorbox-min.js"></script>
     <script>
       $( document ).ready(function() {
-        $('a.brood-colorbox').colorbox({inline:true});
+        $('a.brood-colorbox').colorbox({
+          inline:true,
+          maxWidth:"90%"
+        });
+      });
+    </script>
+    <script>
+      $( document ).ready(function() {
+        $('a.colorbox').colorbox({
+          // rel:$(this).attr('src'),
+          rel:"colorbox",
+          innerWidth:"90%",
+          innerHeight:"90%"
+        });
       });
     </script>
 
