2387a18e6f62c64dd08e97d2e350e459dd579da1
Vladimir Gusev <vpacifist@yandex.ru>
2016-05-22 11:47:03 +0300

    Сделал нормальные контакты

--- www/contacts.php
+++ www/contacts.php
@@ -3,10 +3,11 @@
     <main>
       <div class="container">
         <h1>Контакты</h1>
-        <p>Текст с контактами</p>
-        <a href="http://vk.com" class="block">Ссылка на контакты первая</a>
-        <a href="#" class="block">Ссылка на контакты вторая</a>
-        <a href="#" class="block">Ссылка на контакты третья</a>
+        <p>Наш питомник расположен в курортной зоне Санкт-Петербурга. Если у Вас есть вопросы или Вам понравился котенок, обращайтесь удобным для Вас способом:</p>
+        <a href="mailto:olga@karabasville.ru" class="block  contacts-link  link-mail">olga@karabasville.ru</a>
+        <a href="https://www.facebook.com/profile.php?id=100010328652100" class="block  contacts-link  link-fb">Ольга Журавлёва</a>
+        <a href="tel:+79213738373" class="block  contacts-link  link-tel">+7 921 373-83-73</a>
+        <p>С уважением, Ольга.</p>
       </div>
     </main>
 
