836c3dc7866aa4462e18d6ea18239c36a3b79555
Vladimir Gusev <vpacifist@yandex.ru>
2016-05-22 10:57:39 +0300

    Добавил альбом Улыбнись

---
+++ www/album-smile.php
@@ -0,0 +1,69 @@
+<?php include "header.php" ?>
+
+    <main>
+      <div class="container album-smile">
+        <h1>Улыбнись!</h1>
+        <div class="row">
+          <div class="col-lg-3"><a href="img/gallery/smile/01.jpg" class="colorbox"><img src="img/gallery/smile/01.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/02.jpg" class="colorbox"><img src="img/gallery/smile/02.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/03.jpg" class="colorbox"><img src="img/gallery/smile/03.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/04.jpg" class="colorbox"><img src="img/gallery/smile/04.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/05.jpg" class="colorbox"><img src="img/gallery/smile/05.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/06.jpg" class="colorbox"><img src="img/gallery/smile/06.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/07.jpg" class="colorbox"><img src="img/gallery/smile/07.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/08.jpg" class="colorbox"><img src="img/gallery/smile/08.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/09.jpg" class="colorbox"><img src="img/gallery/smile/09.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/10.jpg" class="colorbox"><img src="img/gallery/smile/10.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/11.jpg" class="colorbox"><img src="img/gallery/smile/11.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/12.jpg" class="colorbox"><img src="img/gallery/smile/12.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/13.jpg" class="colorbox"><img src="img/gallery/smile/13.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/14.jpg" class="colorbox"><img src="img/gallery/smile/14.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/15.jpg" class="colorbox"><img src="img/gallery/smile/15.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/16.jpg" class="colorbox"><img src="img/gallery/smile/16.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/17.jpg" class="colorbox"><img src="img/gallery/smile/17.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/18.jpg" class="colorbox"><img src="img/gallery/smile/18.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/19.jpg" class="colorbox"><img src="img/gallery/smile/19.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/20.jpg" class="colorbox"><img src="img/gallery/smile/20.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/21.jpg" class="colorbox"><img src="img/gallery/smile/21.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/22.jpg" class="colorbox"><img src="img/gallery/smile/22.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/23.jpg" class="colorbox"><img src="img/gallery/smile/23.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/24.jpg" class="colorbox"><img src="img/gallery/smile/24.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/25.jpg" class="colorbox"><img src="img/gallery/smile/25.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/26.jpg" class="colorbox"><img src="img/gallery/smile/26.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/27.jpg" class="colorbox"><img src="img/gallery/smile/27.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/28.jpg" class="colorbox"><img src="img/gallery/smile/28.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/29.jpg" class="colorbox"><img src="img/gallery/smile/29.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/30.jpg" class="colorbox"><img src="img/gallery/smile/30.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/31.jpg" class="colorbox"><img src="img/gallery/smile/31.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/32.jpg" class="colorbox"><img src="img/gallery/smile/32.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/33.jpg" class="colorbox"><img src="img/gallery/smile/33.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/34.jpg" class="colorbox"><img src="img/gallery/smile/34.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/35.jpg" class="colorbox"><img src="img/gallery/smile/35.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/36.jpg" class="colorbox"><img src="img/gallery/smile/36.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/37.jpg" class="colorbox"><img src="img/gallery/smile/37.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/38.jpg" class="colorbox"><img src="img/gallery/smile/38.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/39.jpg" class="colorbox"><img src="img/gallery/smile/39.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/40.jpg" class="colorbox"><img src="img/gallery/smile/40.jpg" alt="photo"></a></div>
+          <div class="col-lg-3"><a href="img/gallery/smile/41.jpg" class="colorbox"><img src="img/gallery/smile/41.jpg" alt="photo"></a></div>
+
+
+        </div>
+      </div>
+    </main>
+
+    <?php include "footer.php" ?>
+
+    <script src="js/jquery-1.12.0.min.js"></script>
+    <script src="js/jquery.colorbox-min.js"></script>
+    <script>
+      $( document ).ready(function() {
+        $('a.colorbox').colorbox({
+          rel:"colorbox",
+          innerWidth:"90%",
+          innerHeight:"90%"
+        });
+      });
+    </script>
+
+  </body>
+</html>
--- www/gallery.php
+++ www/gallery.php
@@ -3,28 +3,26 @@
     <main>
       <div class="container">
         <h1>Галерея</h1>
-        <h2>Много альбомов с фотографиями котов</h2>
         <div class="row">
-
-          <div class="col-lg-2">
+          <div class="col-lg-4">
+            <p><b>Улыбнись!</b></p>
+            <a href="album-smile.php" >
+              <img src="img/gallery/smile/01.jpg" alt="Имя фотографии">
+            </a>
+          </div>
+<!--           <div class="col-lg-4">
             <p>Имя альбома</p>
             <a href="album-demo.php" >
               <img src="img/cat-demo1.jpg" alt="Имя фотографии">
             </a>
           </div>
-          <div class="col-lg-2">
+          <div class="col-lg-4">
             <p>Имя альбома</p>
             <a href="album-demo.php" >
               <img src="img/cat-demo1.jpg" alt="Имя фотографии">
             </a>
           </div>
-          <div class="col-lg-2">
-            <p>Имя альбома</p>
-            <a href="album-demo.php" >
-              <img src="img/cat-demo1.jpg" alt="Имя фотографии">
-            </a>
-          </div>
-
+ -->
         </div>
       </div>
     </main>
