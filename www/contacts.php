ee4020a6ea466c88aa915367ca9307ac974724e5
Vladimir Gusev <vpacifist@yandex.ru>
2016-05-24 10:33:40 +0300

    Попытка сделать форму контактов

--- www/contacts.php
+++ www/contacts.php
@@ -7,11 +7,78 @@
         <a href="mailto:olga@karabasville.ru" class="block  contacts-link  link-mail">olga@karabasville.ru</a>
         <a href="https://www.facebook.com/profile.php?id=100010328652100" class="block  contacts-link  link-fb">Ольга Журавлёва</a>
         <a href="tel:+79213738373" class="block  contacts-link  link-tel">+7 921 373-83-73</a>
-        <p>С уважением, Ольга.</p>
+        <form action="mail.php" method="post" onsubmit="return checkForm(this)" class="contacts-form">
+          <label required><span class="required-field">Имя:</span><input type="text" name="name" ></label>
+          <label required><span class="required-field">e-mail:</span><input type="email" name="email" ></label>
+          <label required><span class="required-field">Текст письма:</span><textarea class="required-field" name="mess" id=""></textarea></label>
+          <input type="submit" id="contacts-submit">
+        </form>
       </div>
     </main>
 
     <?php include "footer.php" ?>
+
+<!--     <script type="text/javascript">
+      function checkForm(obj){
+        var return_value = true;
+        // заносим значение поля почтовый ящик в переменную mail
+        var mail = obj.email.value;
+        // заносим значение поля отправитель в переменную sender
+        var sender = obj.name.value;
+        // заносим значение поля сообщение в переменную msg
+        var msg = obj.mess.value;
+        // регулярное выражение для проверки почтового ящика
+        var reg_mail = /[0-9a-z_]+@[0-9a-z_^.]+.[a-z]{2,3}/i;
+        // регулярное выражение для проверки отправителя (буква a - латинская)
+        var reg_sender = /[a-я,0-9]/i;
+        // объявляем переменную, куда будет заноситься текст сообщения об ошибке
+        var error_msg = "Некорректно заполнены поля:\n";
+        //проверка поля отправитель
+        if(reg_sender.exec(sender) == null){
+          error_msg += "Ваше имя\n";
+          return_value = false;
+        }
+        //проверка поля почтовый ящик
+        if(reg_mail.exec(mail) == null){
+          error_msg += "Ваш e-mail\n";
+          return_value = false;
+        }
+        //проверка поля текст сообщения
+        if(msg == ""){
+          error_msg += "Ваше сообщение\n";
+          return_value = false;
+        }
+        //проверка на наличие ошибок, если возникла ошибка, выводим текст сообщения
+        if(!return_value)
+          alert(error_msg);
+        return return_value;
+      }
+    </script>
+ -->    <script>
+        $(document).ready(function(){
+            $('#contacts-submit').click(function(){
+                // собираем данные с формы
+                var name    = $('#name').val();
+                var email   = $('#email').val();
+                var mess    = $('#mess').val();
+                // отправляем данные
+                $.ajax({
+                    url: "mail.php", // куда отправляем
+                    type: "post", // метод передачи
+                    dataType: "json", // тип передачи данных
+                    data: { // что отправляем
+                        "name":    name,
+                        "email":   email,
+                        "mess":    mess
+                    },
+                    // после получения ответа сервера
+                    success: function(data){
+                        $('.messages').html(data.result); // выводим ответ сервера
+                    }
+                });
+            });
+        });
+    </script>
 
 
   </body>
---
+++ www/mail.php
@@ -0,0 +1,77 @@
+<!-- <!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Обратная связь</title>
+  </head>
+  <body> -->
+    <!-- <?php
+      if (isset($_POST['name'])) {$name = $_POST['name'];}
+      if (isset($_POST['email'])) {$email = $_POST['email'];}
+      if (isset($_POST['mess'])) {$mess = $_POST['mess'];}
+
+      $to = "olga@karabasville.ru"; /*Укажите ваш адрес электоронной почты*/
+      $headers = "Content-type: text/plain; charset = utf-8";
+      $subject = "Сообщение с вашего сайта";
+      $message = "Имя пославшего: $name \nЭлектронный адрес: $email \nСообщение: $mess";
+      $send = mail ($to, $subject, $message, $headers);
+      if ($send == 'true')
+        {
+          echo "Ваше сообщение отправлено, спасибо!";
+          include "contacts.php";
+          //echo "<a href=index.html>Нажмите,</a> чтобы вернуться на главную страницу";
+        }
+        else
+          { echo "Ошибка. Сообщение не отправлено!"; }
+    ?> -->
+
+<?php
+    $msg_box = ""; // в этой переменной будем хранить сообщения формы
+    $errors = array(); // контейнер для ошибок
+    // проверяем корректность полей
+    if($_POST['name'] == "")    $errors[] = "Поле 'Ваше имя' не заполнено!";
+    if($_POST['email'] == "")   $errors[] = "Поле 'Ваш e-mail' не заполнено!";
+    if($_POST['mess'] == "")    $errors[] = "Поле 'Текст сообщения' не заполнено!";
+
+    // если форма без ошибок
+    if(empty($errors)){
+        // собираем данные из формы
+        $message  = "Имя пользователя: " . $_POST['name'] . "<br/>";
+        $message .= "E-mail пользователя: " . $_POST['email'] . "<br/>";
+        $message .= "Текст письма: " . $_POST['mess'];
+        send_mail($message); // отправим письмо
+        // выведем сообщение об успехе
+        $msg_box = "<span style='color: green;'>Сообщение успешно отправлено!</span>";
+    }else{
+        // если были ошибки, то выводим их
+        $msg_box = "";
+        foreach($errors as $one_error){
+            $msg_box .= "<span style='color: red;'>$one_error</span><br/>";
+        }
+    }
+
+    // делаем ответ на клиентскую часть в формате JSON
+    echo json_encode(array(
+        'result' => $msg_box
+    ));
+
+
+    // функция отправки письма
+    function send_mail($message){
+        // почта, на которую придет письмо
+        $mail_to = "olga@karabasville.ru";
+        // тема письма
+        $subject = "Письмо с сайта karabasville.ru";
+
+        // заголовок письма
+        $headers= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-type: text/html; charset=utf-8\r\n"; // кодировка письма
+        $headers .= "From: Тестовое письмо <no-reply@test.com>\r\n"; // от кого письмо
+
+        // отправляем письмо
+        mail($mail_to, $subject, $message, $headers);
+    }
+
+?>
+<!--   </body>
+</html> -->
