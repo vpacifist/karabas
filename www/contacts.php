4482fe8f0359690b7367863ddafceecdad31f3f5
Vladimir Gusev <vpacifist@yandex.ru>
2016-05-25 08:44:25 +0300

    Сделал форму обратной связи в Контакты

--- www/contacts.php
+++ www/contacts.php
@@ -7,55 +7,22 @@
         <a href="mailto:olga@karabasville.ru" class="block  contacts-link  link-mail">olga@karabasville.ru</a>
         <a href="https://www.facebook.com/profile.php?id=100010328652100" class="block  contacts-link  link-fb">Ольга Журавлёва</a>
         <a href="tel:+79213738373" class="block  contacts-link  link-tel">+7 921 373-83-73</a>
-        <form action="mail.php" method="post" onsubmit="return checkForm(this)" class="contacts-form">
-          <label required><span class="required-field">Имя:</span><input type="text" name="name" ></label>
-          <label required><span class="required-field">e-mail:</span><input type="email" name="email" ></label>
-          <label required><span class="required-field">Текст письма:</span><textarea class="required-field" name="mess" id=""></textarea></label>
-          <input type="submit" id="contacts-submit">
         <a href="skype:olgazhur66" class="block  contacts-link  link-skype">olgazhur66</a>
+
+        <form action="" class="contacts-form">
+          <label ><span class="required-field">Имя:</span><input type="text" id="name" required></label>
+          <label ><span class="required-field">e-mail:</span><input type="email" id="email" required></label>
+          <label ><span class="required-field">Текст письма:</span><textarea id="mess" id="" required></textarea></label>
+          <input type="button" id="contacts-submit" value="Отправить" class="input-send">
         </form>
+        <div class="contacts-alerts"></div>
       </div>
     </main>
 
     <?php include "footer.php" ?>
 
-<!--     <script type="text/javascript">
-      function checkForm(obj){
-        var return_value = true;
-        // заносим значение поля почтовый ящик в переменную mail
-        var mail = obj.email.value;
-        // заносим значение поля отправитель в переменную sender
-        var sender = obj.name.value;
-        // заносим значение поля сообщение в переменную msg
-        var msg = obj.mess.value;
-        // регулярное выражение для проверки почтового ящика
-        var reg_mail = /[0-9a-z_]+@[0-9a-z_^.]+.[a-z]{2,3}/i;
-        // регулярное выражение для проверки отправителя (буква a - латинская)
-        var reg_sender = /[a-я,0-9]/i;
-        // объявляем переменную, куда будет заноситься текст сообщения об ошибке
-        var error_msg = "Некорректно заполнены поля:\n";
-        //проверка поля отправитель
-        if(reg_sender.exec(sender) == null){
-          error_msg += "Ваше имя\n";
-          return_value = false;
-        }
-        //проверка поля почтовый ящик
-        if(reg_mail.exec(mail) == null){
-          error_msg += "Ваш e-mail\n";
-          return_value = false;
-        }
-        //проверка поля текст сообщения
-        if(msg == ""){
-          error_msg += "Ваше сообщение\n";
-          return_value = false;
-        }
-        //проверка на наличие ошибок, если возникла ошибка, выводим текст сообщения
-        if(!return_value)
-          alert(error_msg);
-        return return_value;
-      }
-    </script>
- -->    <script>
+    <script src="js/jquery-1.12.0.min.js"></script>
+    <script>
         $(document).ready(function(){
             $('#contacts-submit').click(function(){
                 // собираем данные с формы
@@ -74,7 +41,7 @@
                     },
                     // после получения ответа сервера
                     success: function(data){
-                        $('.messages').html(data.result); // выводим ответ сервера
+                        $('.contacts-alerts').html(data.result); // выводим ответ сервера
                     }
                 });
             });
--- www/mail.php
+++ www/mail.php
@@ -1,30 +1,3 @@
-<!-- <!DOCTYPE html>
-<html>
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>Обратная связь</title>
-  </head>
-  <body> -->
-    <!-- <?php
-      if (isset($_POST['name'])) {$name = $_POST['name'];}
-      if (isset($_POST['email'])) {$email = $_POST['email'];}
-      if (isset($_POST['mess'])) {$mess = $_POST['mess'];}
-
-      $to = "olga@karabasville.ru"; /*Укажите ваш адрес электоронной почты*/
-      $headers = "Content-type: text/plain; charset = utf-8";
-      $subject = "Сообщение с вашего сайта";
-      $message = "Имя пославшего: $name \nЭлектронный адрес: $email \nСообщение: $mess";
-      $send = mail ($to, $subject, $message, $headers);
-      if ($send == 'true')
-        {
-          echo "Ваше сообщение отправлено, спасибо!";
-          include "contacts.php";
-          //echo "<a href=index.html>Нажмите,</a> чтобы вернуться на главную страницу";
-        }
-        else
-          { echo "Ошибка. Сообщение не отправлено!"; }
-    ?> -->
-
 <?php
     $msg_box = ""; // в этой переменной будем хранить сообщения формы
     $errors = array(); // контейнер для ошибок
@@ -73,5 +46,3 @@
     }
 
 ?>
-<!--   </body>
-</html> -->
